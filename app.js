379de3fd79797f9a2a21241d6ecba0aa50927f9c
Piyush Raj <kasbapiyush@gmail.com>
2025-04-11 09:07:54 +0530

    Add start script and server config

--- app.js
+++ app.js
@@ -215,8 +215,5 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
