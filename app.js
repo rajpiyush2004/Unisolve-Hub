ef223e50a2ff97086a478e21e84e9ddf96d17858
Piyush Raj <kasbapiyush@gmail.com>
2025-04-11 08:58:48 +0530

    Update start script and bind app.js

--- app.js
+++ app.js
@@ -215,5 +215,8 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
